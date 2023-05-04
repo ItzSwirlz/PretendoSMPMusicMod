afaa8c87d79eded9db84e15bcef94d81d1855848
Joshua Peisach <itzswirlz2020@outlook.com>
2023-05-04 13:23:27 -0400

    Play our music through Minecraft's Music Manager (big deal!)
    Read the comments - this *should* allow us to stop the music
    and manipuate it more.

--- src/main/java/com/github/ItzSwirlz/PretendoSMPMusic/PretendoSMPMusicMod.java
+++ src/main/java/com/github/ItzSwirlz/PretendoSMPMusic/PretendoSMPMusicMod.java
@@ -2,6 +2,7 @@ package com.github.ItzSwirlz.PretendoSMPMusic;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.Music;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -51,7 +52,19 @@ public class PretendoSMPMusicMod
         @SubscribeEvent
         public static void onPlayerEvent(TickEvent.PlayerTickEvent event) {
             if(Minecraft.getInstance().getCurrentServer() != null && Minecraft.getInstance().getCurrentServer().ip.equals("survival.pretendo.network")) {
-                event.player.playSound(WARAWARAPLAZA.get());
+                /* We can actually inject the music into the music manager.
+                   The 'true' statement means this *will replace currently playing music*
+                   This also means the sounds can be adjusted.
+                   Additionally, if the sound volume changes in settings, so will the music.
+                   Ideally I would make this ambient but I don't feel like digging through the SoundInstance
+
+                   One more thing - the Music can't be initialized as its own variable, for some reason MC crashes
+                   unless the Music is being registered when trying to also start playing music. Probably because
+                   of the main menu music, the game trying to play a sound that may not have been registered.
+                */
+                if(!Minecraft.getInstance().getMusicManager().isPlayingMusic(new Music(WARAWARAPLAZA.getHolder().get(), 0, 0, true))) {
+                    Minecraft.getInstance().getMusicManager().startPlaying(new Music(WARAWARAPLAZA.getHolder().get(), 0, 0, true));
+                }
             }
         }
     }
