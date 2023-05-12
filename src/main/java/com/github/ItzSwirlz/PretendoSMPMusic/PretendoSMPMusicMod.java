ee8a0ef197cf8ad8ffad41e441fc52c565ada01c
Joshua Peisach <itzswirlz2020@outlook.com>
2023-05-12 16:41:34 -0400

    (Important/Big change): Play sounds through their own instances
    Read code comments for more.

--- src/main/java/com/github/ItzSwirlz/PretendoSMPMusic/PretendoSMPMusicMod.java
+++ src/main/java/com/github/ItzSwirlz/PretendoSMPMusic/PretendoSMPMusicMod.java
@@ -1,43 +1,47 @@
 package com.github.ItzSwirlz.PretendoSMPMusic;
 
+import com.github.ItzSwirlz.PretendoSMPMusic.soundinstance.WaraWaraPlazaSoundInstance;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.registry.Holder;
-import net.minecraft.sound.MusicSound;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.quiltmc.qsl.entity.event.api.client.ClientEntityTickCallback;
 
 public class PretendoSMPMusicMod implements ModInitializer {
-	public static final SoundEvent WARAWARAPLAZA = SoundEvent.createVariableRangeEvent(new Identifier("pretendosmpmusic:music.warawaraplaza"));
-	public static final Holder<SoundEvent> WARAWARAPLAZA_MUSIC = Holder.createDirect(WARAWARAPLAZA);
+	public static final WaraWaraPlazaSoundInstance WARAWARAPLAZA_SOUNDINSTANCE = new WaraWaraPlazaSoundInstance();
 	@Override
 	public void onInitialize(ModContainer mod) {
 		ClientEntityTickCallback.EVENT.register((entity, isPassengerTick) -> {
 			if(isPassengerTick) return;
 			if(entity.isPlayer() && MinecraftClient.getInstance().getCurrentServerEntry() != null && MinecraftClient.getInstance().getCurrentServerEntry().address.equals("survival.pretendo.network")) {
-                /* We can actually inject the music into the music manager.
-                   The 'true' statement means this *will replace currently playing music*
-                   This also means the sounds can be adjusted.
-                   Additionally, if the sound volume changes in settings, so will the music.
-                   Ideally I would make this music but I don't feel like digging through the SoundInstance
+                /* We can actually inject the music into the sound manager.
+                   The music manager is an easier way but this will let the
+                   music play, even if the music slider is disabled.
+                   This will only not play if Ambient/Environment sounds are disabled.
 
-                   Something weird is that when detecting the music, I can't use isPlayingType() on the
-                   MusicSound if its registered before hand. It's fine with the handler, but having a predefined
-                   MusicSound does nothing and the music tracker doesn't recognize it. Maybe its different in memory?
+                   The obvious alternative is to just play it via the client world, but
+                   that is mostly done for item usage sounds (like tools breaking, sharing,
+                   cows; not music that is not to be used with the game's main music but
+                   only for certain scenarios). Thus, those sounds don't have a SoundInstance
+                   and don't require a need to have the sound manager queued to stop the sound
+                   However, we do.
+
+                   In order to do this, each sound gets its own Instance, where then game
+                   can play the audio in its sound manager.
+
+                   In terms of fading in and out, this will probably require
+                   progressively lowering the volume of the audio.
                 */
 
                 /*  ------------------------------------
                               Wara Wara Plaza
                     ------------------------------------ */
 				if(entity.isPlayer() && (-200.0 <= entity.getX() && entity.getX() <= -15.0) && (-115.0 <= entity.getZ() && entity.getZ() <= 150.0) && entity.getY() >= 70) {
-					if (!MinecraftClient.getInstance().getMusicTracker().isPlayingType(new MusicSound(WARAWARAPLAZA_MUSIC, 0, 0, true))) {
-						MinecraftClient.getInstance().getMusicTracker().play(new MusicSound(WARAWARAPLAZA_MUSIC, 0, 0, true));
+					if(!MinecraftClient.getInstance().getSoundManager().isPlaying(WARAWARAPLAZA_SOUNDINSTANCE)) {
+						MinecraftClient.getInstance().getSoundManager().play(WARAWARAPLAZA_SOUNDINSTANCE);
 					}
 				} else {
-					if (MinecraftClient.getInstance().getMusicTracker().isPlayingType(new MusicSound(WARAWARAPLAZA_MUSIC, 0, 0, true))) {
-						MinecraftClient.getInstance().getMusicTracker().stop();
+					if (MinecraftClient.getInstance().getSoundManager().isPlaying(WARAWARAPLAZA_SOUNDINSTANCE)) {
+						MinecraftClient.getInstance().getSoundManager().stop(WARAWARAPLAZA_SOUNDINSTANCE);
 					}
 				}
 			}
---
+++ src/main/java/com/github/ItzSwirlz/PretendoSMPMusic/soundinstance/WaraWaraPlazaSoundInstance.java
@@ -0,0 +1,23 @@
+package com.github.ItzSwirlz.PretendoSMPMusic.soundinstance;
+
+import net.minecraft.client.sound.MovingSoundInstance;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+
+// Based on BiomeEffectSoundPlayer's MusicLoop
+public class WaraWaraPlazaSoundInstance extends MovingSoundInstance {
+    public WaraWaraPlazaSoundInstance() {
+        // Initialize sounds here
+        super(SoundEvent.createVariableRangeEvent(new Identifier("pretendosmpmusic:music.warawaraplaza")), SoundCategory.AMBIENT, SoundInstance.createRandom());
+    }
+
+    @Override
+    public void tick() {
+        // nothing needs to be done here, game will do what it needs to
+        // maybe for a fade out/fade in mechanism this might be required
+        // but the main loop in the main mod class can probably find some
+        // way to progressively set the volume lower
+    }
+}
